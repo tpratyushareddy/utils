8c3802e9508331012f925786e150fd6249e34440
tpratyushareddy <50409898+tpratyushareddy@users.noreply.github.com>
2024-03-24 21:18:59 -0400

    Added Docker installation check

--- Docker_cleanup.sh
+++ Docker_cleanup.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 set -e # Stop execution of the code if any of the steps fails
 
+echo "Checking docker installation"
+
+if  ! Docker --version 
+then
+    echo "Docker is not installed"
+    exit 1
+else 
+    echo "Docker is installed"
+fi
+
 # Stores the passed argument
 action=$1
 # Comparing the argument with the string
-if [ action == "images" ]
+if [ $action == "images" ]
 then
     echo "Removing Images"
     #rmi is used to delete the images and docker images -q gives all the docker image ids. 
     docker rmi -f $(docker images -q)
-elif [ action == "containers" ]
+elif [ $action == "containers" ]
 then
     echo "Removing Containers"
     docker rm -f $(docker ps -aq)
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # utils
 
+This Program First Checks if Docker is installed or not. 
+If the Docker is present based on the argument passed, it will either delete all images or containers. 
+If the passed argument is not recognized by the program, an error message is prompted. 
+
 ## To run script
 bash Docker_cleaup.sh {pass an argument}
